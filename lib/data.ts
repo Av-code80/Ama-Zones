67c1cfe3c6872bffa506a23d1daa32a38e18e1a1
Av-code80 <dev.avehghasemian@gmail.com>
2025-02-24 13:13:18 +0100

    feat: :sparkles: Implement carousel in homeLayout

--- lib/data.ts
+++ lib/data.ts
@@ -39,5 +39,28 @@ const data = {
       href: '/page/help',
     },
   ],
+  carousels: [
+    {
+      title: 'Most Popular Shoes For Sale',
+      buttonCaption: 'Shop Now',
+      images: '/images/banner3.jpg',
+      url: '/search?category=Shoes',
+      isPublished: true,
+    },
+    {
+      title: 'Best Sellers in T-Shirts',
+      buttonCaption: 'Shop Now',
+      images: '/images/banner1.jpg',
+      url: 'search?category=T-Shirts',
+      isPublished: true,
+    },
+    {
+      title: 'Best Deals on Wrist Watches',
+      buttonCaption: 'See More',
+      images: '/images/banner2.jpg',
+      url: 'search?category=Wrist Watches',
+      isPublished: true,
+    },
+  ],
 }
 export default data

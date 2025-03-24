51cb85657ab0380060574dae0ec5640830550783
Av-code80 <dev.avehghasemian@gmail.com>
2025-03-24 09:14:59 +0100

    feat: :sparkles: Implement pay order feature by paypal

--- emails/index.tsx
+++ emails/index.tsx
@@ -3,6 +3,7 @@ import PurchaseReceiptEmail from './purchase-receipt'
 import { IOrder } from '@/lib/db/models/order.model'
 import { SENDER_EMAIL, SENDER_NAME } from '@/lib/constants'
 
+// Resend. getting API from https://app.re-send.com/api-keys
 const resend = new Resend(process.env.RESEND_API_KEY as string)
 
 export const sendPurchaseReceipt = async ({ order }: { order: IOrder }) => {
